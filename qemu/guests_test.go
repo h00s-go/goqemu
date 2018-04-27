967db29efc86d6fff6384cc829791fd21fe5821f
Krunoslav Husak <kruno@binel.hr>
2018-04-27 13:27:45 +0200

    Added guests test

---
+++ qemu/guests_test.go
@@ -0,0 +1,16 @@
+package qemu
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLoadingAndParsing(t *testing.T) {
+	g, err := Load("../guests.json")
+	if err != nil {
+		t.Error("Error while loading guests", err)
+	}
+	for _, guest := range g {
+		fmt.Println(guest)
+	}
+}
